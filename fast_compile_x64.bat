c5ceb0afb58954973af2fed3d53262560ca360ab
Project ArkAngel <projectaa76@gmail.com>
2022-06-12 19:27:49 -0400

    Prelimnaries for fixing #1, updating some ignores, and fixing some compile bats

--- fast_compile_x64.bat
+++ fast_compile_x64.bat
@@ -1,4 +1,4 @@
 @echo off
 
 echo Buidling TerraAngel as x64
-dotnet build src\TerraAngel\Terraria\Terraria.csproj -p:Configuration=Release;Platform=x64 > build_log_x64.txt
+dotnet build src\TerraAngel\Terraria\Terraria.csproj -p:Configuration=Release;Platform=x64
--- fast_compile_x86.bat
+++ fast_compile_x86.bat
@@ -1,4 +1,4 @@
 @echo off
 
 echo Buidling TerraAngel as x86
-dotnet build src\TerraAngel\Terraria\Terraria.csproj -p:Configuration=Release;Platform=x86 > build_log_x86.txt
+dotnet build src\TerraAngel\Terraria\Terraria.csproj -p:Configuration=Release;Platform=x86
