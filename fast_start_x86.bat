faa904548f397132c0124c1e3ad4dba46df43a23
Project ArkAngel <projectaa76@gmail.com>
2022-06-12 16:32:02 -0400

    Add git submodule update to bat scripts

--- fast_start_x86.bat
+++ fast_start_x86.bat
@@ -1,6 +1,7 @@
 @echo off
 
 echo Building TerraAngelSetup
+git submodule update > NUL
 dotnet build TerraAngelSetup\TerraAngelSetup\TerraAngelSetup.csproj -c=Release > NUL
 
 echo Running TerraAngelSetup
