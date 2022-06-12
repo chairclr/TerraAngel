6f69bba5c32f13c3e216ccff76955487c5b0866d
Project ArkAngel <projectaa76@gmail.com>
2022-06-12 12:24:54 -0400

    Un-ignore LibNew/x64 and LibNew/x86

--- fast_start_x86.bat
+++ fast_start_x86.bat
@@ -7,7 +7,7 @@ echo Running TerraAngelSetup
 TerraAngelSetup\TerraAngelSetup\bin\Release\net6.0\TerraAngelSetup.exe -auto -patchinput TerraAngelSetup\TerraAngelSetup\Patches\TerraAngelPatches
 
 echo Buidling TerraAngel as x86
-dotnet build src\TerraAngel\Terraria\Terraria.csproj -p:Configuration=Release;Platform=x86  > NUL
+dotnet build src\TerraAngel\Terraria\Terraria.csproj -p:Configuration=Release;Platform=x86 > build_log_x86.txt
 
 echo Running TerraAngelSetup
 TerraAngelSetup\TerraAngelSetup\bin\Release\net6.0\TerraAngelSetup.exe -prebuild -patchinput TerraAngelSetup\TerraAngelSetup\Patches\TerraAngelPatches -compileroutput src\TerraAngel\Terraria\bin\x86\Release\net6.0
