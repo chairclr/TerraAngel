134b2e9e75140321303c18ae25de00d16d8be325
CEO-Chair <87276335+CEO-Chair@users.noreply.github.com>
2022-06-13 23:21:09 -0400

    fix garbage

--- fast_start_x64.bat
+++ fast_start_x64.bat
@@ -5,7 +5,7 @@ git submodule update --remote --recursive > NUL
 dotnet build TerraAngelSetup\TerraAngelSetup\TerraAngelSetup.csproj -c=Release > NUL
 
 echo Running TerraAngelSetup
-TerraAngelSetup\TerraAngelSetup\bin\Release\net6.0\TerraAngelSetup.exe -auto -patchinput Patches\TerraAngelPatches\
+TerraAngelSetup\TerraAngelSetup\bin\Release\net6.0\TerraAngelSetup.exe -auto -patchinput TerraAngelPatches\
 
 echo Buidling TerraAngel as x64
 dotnet build src\TerraAngel\Terraria\Terraria.csproj -p:Configuration=Release;Platform=x64 > build_log_x64.txt
