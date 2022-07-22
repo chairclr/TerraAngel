1b81f0bd22f05c668a99cb5c60922da72f8c21ba
Project ArkAngel <projectaa76@gmail.com>
2022-07-21 20:14:33 -0400

    add sexer

---
+++ TerraAngelPatches/Terraria/TerraAngel/Sexer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerraAngel
+{
+    class Sexer
+    {
+        public const string SexerName = "DefaultSexer";
+
+        public static Sexer CreateSexer()
+        {
+            return new Sexer();
+        }
+
+        public Sexer()
+        {
+
+        }
+    }
+}
