beef03c896769c92156b1f4003f0ebd507ee331f
Project ArkAngel <projectaa76@gmail.com>
2022-08-17 01:52:08 -0400

    change tiles to use unmanaged memory

--- TerraAngelPatches/Terraria/Tilemap.cs
+++ TerraAngelPatches/Terraria/Tilemap.cs
@@ -2,24 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Terraria
 {
-    public class Tilemap
+    public unsafe class Tilemap
     {
         public readonly ushort Width;
         public readonly ushort Height;
 
-        public readonly Tile?[,] tiles;
+        //public readonly Tile?[,] tiles;
+
+        public readonly TileData* tiles;
 
         public Tilemap(int width, int height)
         {
             Width = (ushort)width;
             Height = (ushort)height;
 
-            tiles = new Tile[Width, Height];
+            //tiles = new Tile[Width, Height];
+            tiles = (TileData*)Marshal.AllocHGlobal(Width * Height * sizeof(TileData));
         }
 
         public Tile? this[int x, int y]
@@ -31,11 +35,14 @@ namespace Terraria
                 {
                     return null;
                 }
-                return tiles[x, y];
+                return new Tile(tiles + (x + (y * Width)));
             }
             set
             {
-                tiles[x, y] = value;
+                //tiles[x, y] = value;
+                //
+                //tiles[x + (y * Width)] = value.Data;
+                Buffer.MemoryCopy(value.Data, tiles + (x + (y * Width)), sizeof(TileData), sizeof(TileData));
             }
         }
     }
