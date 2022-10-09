d3acb9c32f5bfdff40dccd575e32aa32d5a3b348
Project ArkAngel <projectaa76@gmail.com>
2022-10-09 12:17:07 -0400

    remove copy-content bat

--- fast_start_copy_content.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-echo Building TerraAngelSetup
-git submodule update --init --remote --recursive > NUL
-dotnet build TerraAngelSetup\TerraAngelSetup\TerraAngelSetup.csproj -c=Release > NUL
-
-echo Running TerraAngelSetup
-TerraAngelSetup\TerraAngelSetup\bin\Release\net6.0\TerraAngelSetup.exe -auto -patchinput TerraAngelPatches\
