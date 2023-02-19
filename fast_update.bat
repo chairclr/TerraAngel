b7f9f6b93ebc6fd756c9fae9dda8eb0f498cf608
chair <projectaa76@gmail.com>
2023-02-19 14:59:36 -0500

    improve `fast_*.bat` files
    Change all `fast_*.bat` files that execute TerraAngelSetup to only compile TerraAngelSetup if it doesn't already exist

--- fast_update.bat
+++ fast_update.bat
@@ -3,9 +3,11 @@
 echo Pulling changes
 git pull > NUL
 
-echo Building TerraAngelSetup
-git submodule update --remote --recursive > NUL
-dotnet build TerraAngelSetup\TerraAngelSetup\TerraAngelSetup.csproj -c=Release > NUL
+if not exist TerraAngelSetup\TerraAngelSetup\bin\Release\net6.0\TerraAngelSetup.exe (
+    echo Building TerraAngelSetup
+    git submodule update --remote --recursive > NUL
+    dotnet build TerraAngelSetup\TerraAngelSetup\TerraAngelSetup.csproj -c=Release > NUL
+)
 
 echo Running TerraAngelSetup -patch
 TerraAngelSetup\TerraAngelSetup\bin\Release\net6.0\TerraAngelSetup.exe -patch -patchinput TerraAngelPatches\
