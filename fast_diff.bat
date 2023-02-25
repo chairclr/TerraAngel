21684aba403e3c71e521a17f2f35919fc3439ca2
chair <projectaa76@gmail.com>
2023-02-25 16:06:09 -0500

    Update `DotnetPatcher` and `TerraAngelSetup` to net7.0
    Update `fast_*.bat` files
    Fixes #36 and #37

--- fast_diff.bat
+++ fast_diff.bat
@@ -1,10 +1,10 @@
 @echo off
 
-if not exist TerraAngelSetup\TerraAngelSetup\bin\Release\net6.0\TerraAngelSetup.exe (
+if not exist TerraAngelSetup\TerraAngelSetup\bin\Release\net7.0\TerraAngelSetup.exe (
     echo Building TerraAngelSetup
     git submodule update --remote --recursive > NUL
     dotnet build TerraAngelSetup\TerraAngelSetup\TerraAngelSetup.csproj -c=Release > NUL
 )
 
 echo Running TerraAngelSetup -diff
-TerraAngelSetup\TerraAngelSetup\bin\Release\net6.0\TerraAngelSetup.exe -diff -patchinput TerraAngelPatches\
+TerraAngelSetup\TerraAngelSetup\bin\Release\net7.0\TerraAngelSetup.exe -diff -patchinput TerraAngelPatches\
