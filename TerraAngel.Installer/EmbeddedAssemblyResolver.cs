07a0a3d735af06d35f57411eeeda3d3eecad2e85
chair <projectaa76@gmail.com>
2023-08-10 23:20:42 -0400

    Implement basic decompilation process

---
+++ TerraAngel.Installer/EmbeddedAssemblyResolver.cs
@@ -0,0 +1,108 @@
+﻿using System.Runtime.Serialization;
+using System.Xml.Linq;
+using ICSharpCode.Decompiler.Metadata;
+
+namespace TerraAngel.Installer;
+
+internal class EmbeddedAssemblyResolver : IAssemblyResolver
+{
+    private readonly PEFile MainModule;
+
+    private readonly Dictionary<string, PEFile> CachedAssemblies = new Dictionary<string, PEFile>();
+
+    private readonly UniversalAssemblyResolver FallbackResolver;
+
+    private readonly IEnumerable<Resource> EmbeddedResources;
+
+    public EmbeddedAssemblyResolver(PEFile mainModule)
+    {
+        MainModule = mainModule;
+
+        FallbackResolver = new UniversalAssemblyResolver(mainModule.FileName, true, mainModule.DetectTargetFrameworkId());
+
+        EmbeddedResources = MainModule.Resources.Where(r => r.ResourceType == ResourceType.Embedded);
+    }
+
+    public PEFile? Resolve(IAssemblyReference reference)
+    {
+        if (CachedAssemblies.TryGetValue(reference.FullName, out PEFile? module))
+        {
+            return module;
+        }
+
+        string resourceName = $"{reference.Name}.dll";
+
+        Resource? res = EmbeddedResources.SingleOrDefault(r => r.Name.EndsWith(resourceName));
+
+        if (res is not null)
+        {
+            module = new PEFile(res.Name, res.TryOpenStream()!);
+            CachedAssemblies.Add(reference.FullName, module);
+            return module;
+        }
+
+        module = FallbackResolver.Resolve(reference);
+        
+        if (module is null)
+        {
+            throw new Exception($"Cound not resolve reference to {reference.FullName}");
+        }
+
+        CachedAssemblies.Add(reference.FullName, module);
+
+        return module;
+    }
+
+    public Task<PEFile?> ResolveAsync(IAssemblyReference reference)
+    {
+        return Task.Run<PEFile?>(() =>
+        {
+            PEFile? module;
+
+            lock (CachedAssemblies)
+            {
+                if (CachedAssemblies.TryGetValue(reference.FullName, out module))
+                {
+                    return module;
+                }
+            }
+
+            string resourceName = $"{reference.Name}.dll";
+
+            Resource? res = EmbeddedResources.SingleOrDefault(r => r.Name.EndsWith(resourceName));
+
+            if (res is not null)
+            {
+                module = new PEFile(res.Name, res.TryOpenStream()!);
+            }
+
+            if (module is null)
+            {
+                module = FallbackResolver.Resolve(reference);
+
+                // If the fallback resolver fails, we're in trouble
+                if (module is null)
+                {
+                    throw new Exception($"Cound not resolve reference to {reference.FullName}");
+                }
+            }
+
+            lock (CachedAssemblies)
+            {
+                CachedAssemblies.Add(reference.FullName, module);
+            }
+
+            return module;
+        });
+    }
+
+    public PEFile? ResolveModule(PEFile mainModule, string moduleName)
+    {
+        return FallbackResolver.ResolveModule(mainModule, moduleName);
+    }
+
+    public Task<PEFile?> ResolveModuleAsync(PEFile mainModule, string moduleName)
+    {
+        return FallbackResolver.ResolveModuleAsync(mainModule, moduleName);
+    }
+}
--- TerraAngel.Installer/Program.cs
+++ TerraAngel.Installer/Program.cs
@@ -1,4 +1,5 @@
-﻿using CommandLine;
+﻿using System.Diagnostics;
+using CommandLine;
 
 namespace TerraAngel.Installer;
 
@@ -9,7 +10,8 @@ internal class Program
         [Option('d', "decomp-target", Required = false, HelpText = "Target Terraria executable for decompilation.")]
         public string? DecompilationTarget { get; set; }
 
-
+        [Option('o', "decomp-output", Required = false, HelpText = "Output directory for decompilation.")]
+        public string DecompilationOutputDirectory { get; set; } = Path.Combine(Environment.CurrentDirectory, "temp", "decomp");
     }
 
     private static void Main(string[] args)
@@ -37,5 +39,13 @@ internal class Program
         }
 
         Console.WriteLine($"Target Terraria executable: {settings.DecompilationTarget}");
+
+        Console.WriteLine($"Decompiling {settings.DecompilationTarget} into {settings.DecompilationOutputDirectory}");
+
+        Stopwatch sw = Stopwatch.StartNew();
+        Decompiler.Decompile(settings.DecompilationTarget, settings.DecompilationOutputDirectory);
+        sw.Stop();
+
+        Console.WriteLine($"Decompiled Terraria. Elapsed: {sw.Elapsed.TotalSeconds}s");
     }
 }
