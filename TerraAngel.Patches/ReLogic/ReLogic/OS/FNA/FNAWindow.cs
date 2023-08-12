b0ccb4d8f3197d412bc2c682098eb9f226ce6922
chair <chairclr@gmail.com>
2023-08-12 16:21:56 -0400

    Make ReLogic build

---
+++ TerraAngel.Patches/ReLogic/ReLogic/OS/FNA/FNAWindow.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace ReLogic.OS.FNA;
+
+internal class FNAWindow : IWindowService
+{
+    public float GetScaling()
+    {
+        return 1f;
+    }
+
+    public void SetQuickEditEnabled(bool enabled)
+    {
+
+    }
+
+    public void SetUnicodeTitle(GameWindow window, string title)
+    {
+        SDL2.SDL.SDL_SetWindowTitle(window.Handle, title);
+    }
+
+    public void StartFlashingIcon(GameWindow window)
+    {
+        SDL2.SDL.SDL_FlashWindow(window.Handle, SDL2.SDL.SDL_FlashOperation.SDL_FLASH_BRIEFLY);
+    }
+
+    public void StopFlashingIcon(GameWindow window)
+    {
+        SDL2.SDL.SDL_FlashWindow(window.Handle, SDL2.SDL.SDL_FlashOperation.SDL_FLASH_CANCEL);
+    }
+}
